fcf1e83972114efe996e6d377ccea197ab0ee80b
djgerah <tarlyjac@student.21-school.ru>
2026-09-10 00:55:08 +0300

    feat: add Allure reporting with steps for API tests

--- src/test/java/qa/startup/skinscan/api/PhotoApiTest.java
+++ src/test/java/qa/startup/skinscan/api/PhotoApiTest.java
@@ -62,10 +62,10 @@ class PhotoApiTest {
 
         Response response = PhotoClient.upload(user, Picture.uniqueName(), Picture.PNG_1X1, Picture.mimeType);
 
-        response.then()
-                .statusCode(202);
-
-        String photoId = response.asString().replace("\"", "").trim();
+        String photoId = response.then()
+                .statusCode(202)
+                .extract()
+                .jsonPath().getString("");
 
         awaitAnalyzed(user, photoId);
 
